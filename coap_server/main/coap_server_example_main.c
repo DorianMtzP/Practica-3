165fd0ca5e2c53c3791cc5a5b43edd11a570b410
DorianMtzP <dorianr.martinez@iteso.mx>
2023-04-23 23:17:57 -0600

    Practica 3 funcionando
    Esta es la version con la que se grabo el video

--- coap_server/main/coap_server_example_main.c
+++ coap_server/main/coap_server_example_main.c
@@ -13,7 +13,10 @@
  * calls.  Any external (to this thread) data transmitted in/out via libcoap
  * therefore has to be passed in/out by xQueue*() via this thread.
  */
-
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <inttypes.h>
 #include <string.h>
 #include <sys/socket.h>
 
@@ -31,6 +34,10 @@
 
 #include "coap3/coap.h"
 
+#include "driver/gpio.h"
+#include "freertos/queue.h"
+
+#include "mdns.h"
 #ifndef CONFIG_COAP_SERVER_SUPPORT
 #error COAP_SERVER_SUPPORT needs to be enabled
 #endif /* COAP_SERVER_SUPPORT */
@@ -57,9 +64,11 @@
 #define EXAMPLE_COAP_LOG_DEFAULT_LEVEL CONFIG_COAP_LOG_DEFAULT_LEVEL
 
 const static char *TAG = "CoAP_server";
+const static char *ISR = "I/O isr";
 
 static char espressif_data[100];
 static int espressif_data_len = 0;
+
 
 #ifdef CONFIG_COAP_MBEDTLS_PKI
 /* CA cert, taken from coap_ca.pem
@@ -82,18 +91,150 @@ extern uint8_t server_key_start[] asm("_binary_coap_server_key_start");
 extern uint8_t server_key_end[]   asm("_binary_coap_server_key_end");
 #endif /* CONFIG_COAP_MBEDTLS_PKI */
 
+#define GPIO_OUTPUT_IO_RUN      13  
+#define GPIO_OUTPUT_IO_ALARM    15
+#define GPIO_OUTPUT_PIN_SEL  ((1ULL<<GPIO_OUTPUT_IO_ALARM) | (1ULL<<GPIO_OUTPUT_IO_RUN))
+#define GPIO_INPUT_IO_ALARM     33 
+#define GPIO_INPUT_IO_RUN    35 //35 ->B2
+#define GPIO_INPUT_PIN_SEL  ((1ULL<<GPIO_INPUT_IO_ALARM) | (1ULL<<GPIO_INPUT_IO_RUN))
+#define ESP_INTR_FLAG_DEFAULT 0
+
+
 #define INITIAL_DATA "Hello World!"
 
+static uint8_t alarm_flag = 0;
+static uint8_t run_flag = 0;
+static int adc_values[10] = { 12, 13, 14, 13, 12, 12, 13, 15, 16, 13};
+static uint8_t adc_count = 0;
+
+static QueueHandle_t gpio_evt_queue = NULL;
+
+// static void blink_led(void)
+// {
+//     /* Set the GPIO level according to the state (LOW or HIGH)*/
+//     gpio_set_level(14, s_led_state);
+// }
+
+static void IRAM_ATTR gpio_isr_handler(void* arg)
+{
+    uint32_t gpio_num = (uint32_t) arg;
+    xQueueSendFromISR(gpio_evt_queue, &gpio_num, NULL);
+}
+
+static void gpio_task_inputs(void* arg)
+{
+    uint32_t io_num;
+    for(;;) {
+        if(xQueueReceive(gpio_evt_queue, &io_num, portMAX_DELAY)) {
+           // printf("GPIO[%"PRIu32"] intr, val: %d\n", io_num, gpio_get_level(io_num));
+            if(io_num == GPIO_INPUT_IO_ALARM){
+               // printf ("entro if GPIO_INPUT_IO_0");
+                alarm_flag  = 1;
+                run_flag = 0;
+                gpio_set_level(GPIO_OUTPUT_IO_ALARM, alarm_flag);
+                gpio_set_level(GPIO_OUTPUT_IO_RUN, run_flag);
+                ESP_LOGI(ISR,"I/O ALARM push button");
+            }else if(io_num == GPIO_INPUT_IO_RUN){
+                //printf ("entro if GPIO_INPUT_IO_0");
+                alarm_flag  = 0;
+                run_flag = 1;
+                gpio_set_level(GPIO_OUTPUT_IO_ALARM, alarm_flag);
+                gpio_set_level(GPIO_OUTPUT_IO_RUN, run_flag);
+                ESP_LOGI(ISR,"I/O RUN push button");
+            }
+        }
+    }
+}
+
+static void configure_IO(void)
+{
+    //zero-initialize the config structure.
+    gpio_config_t io_conf = {};
+    //disable interrupt
+    io_conf.intr_type = GPIO_INTR_DISABLE;
+    //set as output mode
+    io_conf.mode = GPIO_MODE_OUTPUT;
+    //bit mask of the pins that you want to set,e.g.GPIO18/19
+    io_conf.pin_bit_mask = GPIO_OUTPUT_PIN_SEL;
+    //disable pull-down mode
+    io_conf.pull_down_en = 0;
+    //disable pull-up mode
+    io_conf.pull_up_en = 0;
+    //configure GPIO with the given settings
+    gpio_config(&io_conf);
+
+    //interrupt of rising edge
+    io_conf.intr_type = GPIO_INTR_POSEDGE;
+    //bit mask of the pins, use GPIO4/5 here
+    io_conf.pin_bit_mask = GPIO_INPUT_PIN_SEL;
+    //set as input mode
+    io_conf.mode = GPIO_MODE_INPUT;
+    //enable pull-up mode
+    io_conf.pull_up_en = 1;
+    gpio_config(&io_conf);
+
+    //create a queue to handle gpio event from isr
+    gpio_evt_queue = xQueueCreate(10, sizeof(uint32_t));
+    //start gpio task
+    xTaskCreate(gpio_task_inputs, "gpio_task_inputs", 2048, NULL, 10, NULL);
+
+    //install gpio isr service
+    gpio_install_isr_service(ESP_INTR_FLAG_DEFAULT);
+    //hook isr handler for specific gpio pin
+    gpio_isr_handler_add(GPIO_INPUT_IO_ALARM, gpio_isr_handler, (void*) GPIO_INPUT_IO_ALARM);
+    //hook isr handler for specific gpio pin
+    gpio_isr_handler_add(GPIO_INPUT_IO_RUN, gpio_isr_handler, (void*) GPIO_INPUT_IO_RUN);
+
+}
+
 /*
- * The resource handler
+ * Alarm resource handler
  */
 static void
-hnd_espressif_get(coap_resource_t *resource,
+hnd_alarm_get(coap_resource_t *resource,
                   coap_session_t *session,
                   const coap_pdu_t *request,
                   const coap_string_t *query,
                   coap_pdu_t *response)
 {
+
+    ESP_LOGI(TAG, " hnd_alarm_get executed");  
+    gpio_set_level(14, alarm_flag);
+    if(alarm_flag == 1){
+        sprintf(espressif_data,"Generador en Alarma");
+        espressif_data_len = strlen(espressif_data);
+    }
+    else{
+        sprintf(espressif_data,"Generador OK - No Alarmas");
+        espressif_data_len = strlen(espressif_data);
+    }
+    coap_pdu_set_code(response, COAP_RESPONSE_CODE_CONTENT);
+    coap_add_data_large_response(resource, session, request, response,
+                                 query, COAP_MEDIATYPE_TEXT_PLAIN, 60, 0,
+                                 (size_t)espressif_data_len,
+                                 (const u_char *)espressif_data,
+                                 NULL, NULL);
+}
+
+/*
+ * Status (Merged with Coontrol) resource handler
+ */
+static void
+hnd_status_get(coap_resource_t *resource,
+                  coap_session_t *session,
+                  const coap_pdu_t *request,
+                  const coap_string_t *query,
+                  coap_pdu_t *response)
+{
+    ESP_LOGI(TAG, " hnd_status_get executed");
+    if(run_flag == 1){
+        sprintf(espressif_data,"Generador Encendido");
+        espressif_data_len = strlen(espressif_data);
+    }
+    else{
+        sprintf(espressif_data,"Generador Apagado");
+        espressif_data_len = strlen(espressif_data);
+    }
     coap_pdu_set_code(response, COAP_RESPONSE_CODE_CONTENT);
     coap_add_data_large_response(resource, session, request, response,
                                  query, COAP_MEDIATYPE_TEXT_PLAIN, 60, 0,
@@ -103,7 +244,59 @@ hnd_espressif_get(coap_resource_t *resource,
 }
 
 static void
-hnd_espressif_put(coap_resource_t *resource,
+hnd_status_put(coap_resource_t *resource,
+                  coap_session_t *session,
+                  const coap_pdu_t *request,
+                  const coap_string_t *query,
+                  coap_pdu_t *response)
+                  {
+    
+    size_t size;
+    size_t offset;
+    size_t total;
+    const unsigned char *data;
+
+    coap_resource_notify_observers(resource, NULL);
+
+    coap_pdu_set_code(response, COAP_RESPONSE_CODE_CHANGED);
+   // printf("entro hnd_status_put");
+    ESP_LOGI(TAG, " hnd_status_put executed");
+
+    /* coap_get_data_large() sets size to 0 on error */
+    (void)coap_get_data_large(request, &size, &data, &offset, &total);
+
+    if (size == 0) {      /* re-init */
+        run_flag = 0;
+        printf("hnd_status_put size =0");
+        ESP_LOGI(TAG, "hnd_status_put size =0");
+
+    } else {
+        // espressif_data_len = size > sizeof (espressif_data) ? sizeof (espressif_data) : size;
+        // memcpy (espressif_data, data, espressif_data_len);
+        if ('a' == (int)*data) {
+            run_flag = 0;
+            printf("hnd_status_put apagar\n");
+            ESP_LOGI(TAG, "hnd_status_put Apagar Generador");
+           gpio_set_level(GPIO_OUTPUT_IO_RUN, run_flag);     
+        }
+        else if ('e' == (int)*data) {
+            run_flag = 1;
+            printf("hnd_status_put encender\n");
+            ESP_LOGI(TAG, "hnd_status_put Encender Generador");
+            gpio_set_level(GPIO_OUTPUT_IO_RUN, run_flag);
+        }
+        else{
+            printf("Payload not compatible\n");
+            ESP_LOGE(TAG, "Payload not compatible, %d", size);  
+        }
+    }
+}
+
+/*
+ * Reset resource handler
+ */
+static void
+hnd_reset_put(coap_resource_t *resource,
                   coap_session_t *session,
                   const coap_pdu_t *request,
                   const coap_string_t *query,
@@ -113,39 +306,73 @@ hnd_espressif_put(coap_resource_t *resource,
     size_t offset;
     size_t total;
     const unsigned char *data;
+    ESP_LOGI(TAG, " hnd_reset_put executed");
 
     coap_resource_notify_observers(resource, NULL);
 
-    if (strcmp (espressif_data, INITIAL_DATA) == 0) {
-        coap_pdu_set_code(response, COAP_RESPONSE_CODE_CREATED);
-    } else {
-        coap_pdu_set_code(response, COAP_RESPONSE_CODE_CHANGED);
-    }
+    // if (strcmp (espressif_data, INITIAL_DATA) == 0) {
+    //     coap_pdu_set_code(response, COAP_RESPONSE_CODE_CREATED);
+    // } else {
+    coap_pdu_set_code(response, COAP_RESPONSE_CODE_CHANGED);
+    // }
 
     /* coap_get_data_large() sets size to 0 on error */
     (void)coap_get_data_large(request, &size, &data, &offset, &total);
+    alarm_flag = 0 ;
+    run_flag = 0;
+    gpio_set_level(GPIO_OUTPUT_IO_ALARM, alarm_flag);
+    gpio_set_level(GPIO_OUTPUT_IO_RUN, run_flag);
 
-    if (size == 0) {      /* re-init */
-        snprintf(espressif_data, sizeof(espressif_data), INITIAL_DATA);
-        espressif_data_len = strlen(espressif_data);
-    } else {
-        espressif_data_len = size > sizeof (espressif_data) ? sizeof (espressif_data) : size;
-        memcpy (espressif_data, data, espressif_data_len);
-    }
+    // if (size == 0) {      /* re-init */
+    //     snprintf(espressif_data, sizeof(espressif_data), INITIAL_DATA);
+    //     espressif_data_len = strlen(espressif_data);
+    // } else {
+    //     espressif_data_len = size > sizeof (espressif_data) ? sizeof (espressif_data) : size;
+    //     memcpy (espressif_data, data, espressif_data_len);
+    // }
 }
 
+/*
+ * Battery resource handler
+ */
 static void
-hnd_espressif_delete(coap_resource_t *resource,
-                     coap_session_t *session,
-                     const coap_pdu_t *request,
-                     const coap_string_t *query,
-                     coap_pdu_t *response)
+hnd_battery_get(coap_resource_t *resource,
+                  coap_session_t *session,
+                  const coap_pdu_t *request,
+                  const coap_string_t *query,
+                  coap_pdu_t *response)
 {
-    coap_resource_notify_observers(resource, NULL);
-    snprintf(espressif_data, sizeof(espressif_data), INITIAL_DATA);
+    sprintf(espressif_data, "Battery voltage = %d", adc_values[adc_count]);
+    adc_count++;
+    if (adc_count >9){
+        adc_count = 0;
+    }
+    //espressif_data_len = sizeof("Battery voltage = ")+2;
     espressif_data_len = strlen(espressif_data);
-    coap_pdu_set_code(response, COAP_RESPONSE_CODE_DELETED);
+
+    ESP_LOGI(TAG, " hnd_battery_get executed");
+
+    coap_pdu_set_code(response, COAP_RESPONSE_CODE_CONTENT);
+    coap_add_data_large_response(resource, session, request, response,
+                                 query, COAP_MEDIATYPE_TEXT_PLAIN, 60, 0,
+                                 (size_t)espressif_data_len,
+                                 (const u_char *)espressif_data,
+                                 NULL, NULL);
 }
+
+
+// static void
+// hnd_espressif_delete(coap_resource_t *resource,
+//                      coap_session_t *session,
+//                      const coap_pdu_t *request,
+//                      const coap_string_t *query,
+//                      coap_pdu_t *response)
+// {
+//     coap_resource_notify_observers(resource, NULL);
+//     snprintf(espressif_data, sizeof(espressif_data), INITIAL_DATA);
+//     espressif_data_len = strlen(espressif_data);
+//     coap_pdu_set_code(response, COAP_RESPONSE_CODE_DELETED);
+// }
 
 #ifdef CONFIG_COAP_MBEDTLS_PKI
 
@@ -181,20 +408,24 @@ static void coap_example_server(void *p)
 {
     coap_context_t *ctx = NULL;
     coap_address_t serv_addr;
-    coap_resource_t *resource = NULL;
+    coap_resource_t *resource_alarm = NULL;
+    coap_resource_t *resource_control = NULL;
+    coap_resource_t *resource_reset = NULL;
+    coap_resource_t *resource_battery = NULL;
 
     /***********************************
     * If adding mDNS it goes here.
     ***********************************/
-    // mdns_init();
-    // mdns_hostname_set("device_name");
+     mdns_init();
+     mdns_hostname_set("DorianESP32");
 
-    // mdns_txt_item_t serviceTxtData[3] = {
-    //     {"board", "esp32"},
-    //     {"u", "user"},
-    //     {"p","password"}
-    // }
-    // mdns_service_add("device_name", "_coap","_udp",5683,serviceTxData,3);
+     mdns_txt_item_t serviceTxtData[3] = {
+         {"board", "esp32"},
+         {"user", "user"},
+         {"password","password"}
+     };
+     mdns_service_add("DorianESP32", "_coap","_udp",80, serviceTxtData, 3);
+     //mdns_service_add("DORIAN-ESP32", "_coap","_udp",5683, serviceTxtData, 3);
 
     snprintf(espressif_data, sizeof(espressif_data), INITIAL_DATA);
     espressif_data_len = strlen(espressif_data);
@@ -316,17 +547,73 @@ static void coap_example_server(void *p)
             ESP_LOGI(TAG, "MbedTLS (D)TLS Server Mode not configured");
         }
 #endif /* CONFIG_COAP_MBEDTLS_PSK || CONFIG_COAP_MBEDTLS_PKI */
-        resource = coap_resource_init(coap_make_str_const("Espressif"), 0);
-        if (!resource) {
+        
+/*
+*Resource Handlers
+*
+**/
+       // Common alarm
+        /* Here you initialize the resource */
+        resource_alarm = coap_resource_init(coap_make_str_const("Alarm/status"), 0);
+        if (!resource_alarm) {
             ESP_LOGE(TAG, "coap_resource_init() failed");
             goto clean_up;
         }
-        coap_register_handler(resource, COAP_REQUEST_GET, hnd_espressif_get);
-        coap_register_handler(resource, COAP_REQUEST_PUT, hnd_espressif_put);
-        coap_register_handler(resource, COAP_REQUEST_DELETE, hnd_espressif_delete);
+        /* Here is were you register each resource handler (GET,PUT and DELETE) */
+        coap_register_handler(resource_alarm, COAP_REQUEST_GET, hnd_alarm_get);
+        //coap_register_handler(resource_alarm, COAP_REQUEST_PUT, hnd_alarm_put);
+        //coap_register_handler(resource_alarm, COAP_REQUEST_DELETE, hnd_alarm_delete);
         /* We possibly want to Observe the GETs */
-        coap_resource_set_get_observable(resource, 1);
-        coap_add_resource(ctx, resource);
+        coap_resource_set_get_observable(resource_alarm, 1);
+        coap_add_resource(ctx, resource_alarm);
+
+       // Control (merged with status) - Gen running or not
+        /* Here you initialize the resource */
+        resource_control = coap_resource_init(coap_make_str_const("Status"), 0);
+        if (!resource_control) {
+            ESP_LOGE(TAG, "coap_resource_init() failed");
+            goto clean_up;
+        }
+        /* Here is were you register each resource handler (GET,PUT and DELETE) */
+        coap_register_handler(resource_control, COAP_REQUEST_GET, hnd_status_get);
+        coap_register_handler(resource_control, COAP_REQUEST_PUT, hnd_status_put);
+        //coap_register_handler(resource_status, COAP_REQUEST_DELETE, hnd_status_delete);
+        /* We possibly want to Observe the GETs */
+        coap_resource_set_get_observable(resource_control, 1);
+        coap_add_resource(ctx, resource_control);
+       
+       // Alarm Reset
+        /* Here you initialize the resource */
+        resource_reset = coap_resource_init(coap_make_str_const("Alarm/Reset"), 0);
+        if (!resource_reset) {
+            ESP_LOGE(TAG, "coap_resource_init() failed");
+            goto clean_up;
+        }
+        /* Here is were you register each resource handler (GET,PUT and DELETE) */
+        //coap_register_handler(resource_reset, COAP_REQUEST_GET, hnd_espressif_get);
+        coap_register_handler(resource_reset, COAP_REQUEST_PUT, hnd_reset_put);
+        // coap_register_handler(resource_reset, COAP_REQUEST_DELETE, hnd_espressif_delete);
+        /* We possibly want to Observe the GETs */
+        coap_resource_set_get_observable(resource_reset, 1);
+        coap_add_resource(ctx, resource_reset);
+
+
+       //Battery Voltage
+        /* Here you initialize the resource */
+        resource_battery = coap_resource_init(coap_make_str_const("Battery"), 0);
+        if (!resource_battery) {
+            ESP_LOGE(TAG, "coap_resource_init() failed");
+            goto clean_up;
+        }
+        /* Here is were you register each resource handler (GET,PUT and DELETE) */
+        coap_register_handler(resource_battery, COAP_REQUEST_GET, hnd_battery_get);
+        /* We possibly want to Observe the GETs */
+        coap_resource_set_get_observable(resource_battery, 1);
+        coap_add_resource(ctx, resource_battery);
+
+
+
+
 
 #if defined(CONFIG_EXAMPLE_COAP_MCAST_IPV4) || defined(CONFIG_EXAMPLE_COAP_MCAST_IPV6)
         esp_netif_t *netif = NULL;
@@ -369,6 +656,7 @@ clean_up:
 
 void app_main(void)
 {
+    configure_IO();
     ESP_ERROR_CHECK( nvs_flash_init() );
     ESP_ERROR_CHECK(esp_netif_init());
     ESP_ERROR_CHECK(esp_event_loop_create_default());
@@ -378,6 +666,5 @@ void app_main(void)
      * examples/protocols/README.md for more information about this function.
      */
     ESP_ERROR_CHECK(example_connect());
-
     xTaskCreate(coap_example_server, "coap", 8 * 1024, NULL, 5, NULL);
 }
